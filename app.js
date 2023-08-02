c27a88596b26242c8f90983f9702e09cf01a8386
Evgeniy-Moskvin <moskwin.evgeny@gmail.com>
2023-08-02 11:48:15 +0300

    feat: создать схемы и модели

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  /*useCreateIndex: true,
-  useFindAndModify: false*/
 })
   .then(() => {
     console.log('Успешное подключение к БД');
