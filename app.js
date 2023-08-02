03c6c5180b0277df4e987dbc3e81f50311ec2f4e
Evgeniy-Moskvin <moskwin.evgeny@gmail.com>
2023-08-02 11:18:39 +0300

    feat: создать первоначальное приложение

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  /*useCreateIndex: true,
+  useFindAndModify: false*/
+})
+  .then(() => {
+    console.log('Успешное подключение к БД');
+  })
+  .catch(() => {
+    console.error('Ошибка подключения к БД!');
+  });
+
+app.listen(PORT, () => {
+  console.log(`Приложение запущено на порту ${PORT}`);
+});
