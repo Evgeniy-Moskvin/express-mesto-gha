aab11b518d0de07e90898ee73775d1ad4882a97f
Evgeniy-Moskvin <moskwin.evgeny@gmail.com>
2023-08-07 10:18:29 +0300

    feat: добавить email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -17,6 +18,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+
+    validate: {
+      validator: ((v) => {
+        validator.isEmail(v);
+      }),
+      message: 'Некорректный E-mail',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  }
 });
 
 module.exports = mongoose.model('user', userSchema);
