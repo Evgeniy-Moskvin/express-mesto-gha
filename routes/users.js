efa948d57c63035bef8699305c199446208145fa
Evgeniy-Moskvin <moskwin.evgeny@gmail.com>
2023-08-08 11:39:40 +0300

    feat: валидировать запросы для пользователя

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { auth } = require('../middlewares/auth');
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers,
@@ -13,15 +14,41 @@ const {
 
 router.get('/', auth, getUsers);
 
-router.get('/:userId', auth, getUserById);
+router.get('/:userId', auth, celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().required().min(24).max(24).pattern(/[a-z][0-9]+/), //64ca4a49727e7d6dcf4c37bc
+  })
+}), getUserById);
 
-router.post('/signup', createUser);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/(?:http|https):\/\/((?:[\w-]+)(?:\.[\w-]+)+)(?:[\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
-router.patch('/me', auth, updateUser);
+router.patch('/me', auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
 
-router.patch('/me/avatar', auth, updateUserAvatar);
+router.patch('/me/avatar', auth, celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(/(?:http|https):\/\/((?:[\w-]+)(?:\.[\w-]+)+)(?:[\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/),
+  }),
+}), updateUserAvatar);
 
-router.post('/signin', login);
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
 
 router.get('/me', auth, getUser);
 
